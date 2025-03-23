44fcf9ecbcdd991c48b076cf599b66ac754723d4
Cora <enero.noventaytres@gmail.com>
2025-03-23 23:30:50 +0100

    creacion de pages | posible html del formulario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { TuPostComponent } from './pages/tu-post/tu-post.component';
+import { PublicarComponent } from './pages/publicar/publicar.component';
 
 export const routes: Routes = [
-    {path: "", pathMatch:'full', redirectTo: 'home' },
-    {path: 'home', component: HeaderComponent},
-    
+    {path: "", pathMatch:'full', redirectTo: 'home'},
+    {path:'inicio', component: InicioComponent},
+    {path: 'tupost', component: TuPostComponent},
+    {path: 'publicar', component: PublicarComponent},
     { path: "**", redirectTo: 'home' }
 ];
---
+++ src/app/pages/tu-post/tu-post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tu-post',
+  imports: [],
+  templateUrl: './tu-post.component.html',
+  styleUrl: './tu-post.component.css'
+})
+export class TuPostComponent {
+
+}
