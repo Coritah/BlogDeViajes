bada29a92a7f54ca91c38db7c0917e80f6c51328
Cora <enero.noventaytres@gmail.com>
2025-03-24 21:28:26 +0100

    hacer formulario reactivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from "./components/header/header.component";
+import { FormControl } from '@angular/forms';
 
 
 @Component({
@@ -11,5 +12,5 @@ import { HeaderComponent } from "./components/header/header.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+
 }
--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+// IMOPIRTAR SERVICIO
+
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+  // meter el sevicio aqui
+router = inject(Router)
+
+nuevoViajeForm: FormGroup= new FormGroup({
+  id: new FormControl('', [Validators.required]),
+
+  titulo: new FormControl('', [Validators.required]),
+
+  texto: new FormControl('', [Validators.required]),
+
+  autor: new FormControl('', [Validators.required]),
+
+  imagen: new FormControl('',[Validators.required]),
+
+  fecha: new FormControl('', [Validators.required]),
+  
+  categoria: new FormControl('', [Validators.required])
+})
+
+
 
 }
